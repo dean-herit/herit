8e43ea3c1f3b1375586bdcc84dc2516e2facda4d
dean-herit <dean@herit.io>
2025-04-02 16:08:36 +0100

    kinde & shadcdn install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
+import { AuthProvider } from "./AuthProvider";
 
 export const metadata = {
   title: "Herit",
@@ -20,63 +21,67 @@ export default async function RootLayout({
   const { isAuthenticated, getUser } = getKindeServerSession();
   const user = await getUser();
   return (
-    <html lang="en">
-      <body>
-        <header>
-          <nav className="nav container">
-            <h1 className="text-display-3">Herit</h1>
-            <div>
-              {!(await isAuthenticated()) ? (
-                <>
-                  <LoginLink className="btn btn-ghost sign-in-btn">
-                    Sign in
-                  </LoginLink>
-                  <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
-                </>
-              ) : (
-                <div className="profile-blob">
-                  {user?.picture ? (
-                    <img
-                      className="avatar"
-                      src={user?.picture}
-                      alt="user profile avatar"
-                      referrerPolicy="no-referrer"
-                    />
-                  ) : (
-                    <div className="avatar">
-                      {user?.given_name?.[0]}
-                      {user?.family_name?.[0]}
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <nav className="nav container">
+              <h1 className="text-display-3">Herit</h1>
+              <div>
+                {!(await isAuthenticated()) ? (
+                  <>
+                    <LoginLink className="btn btn-ghost sign-in-btn">
+                      Sign in
+                    </LoginLink>
+                    <RegisterLink className="btn btn-dark">
+                      Sign up
+                    </RegisterLink>
+                  </>
+                ) : (
+                  <div className="profile-blob">
+                    {user?.picture ? (
+                      <img
+                        className="avatar"
+                        src={user?.picture}
+                        alt="user profile avatar"
+                        referrerPolicy="no-referrer"
+                      />
+                    ) : (
+                      <div className="avatar">
+                        {user?.given_name?.[0]}
+                        {user?.family_name?.[0]}
+                      </div>
+                    )}
+                    <div>
+                      <p className="text-heading-2">
+                        {user?.given_name} {user?.family_name}
+                      </p>
+
+                      <LogoutLink className="text-subtle">Log out</LogoutLink>
                     </div>
-                  )}
-                  <div>
-                    <p className="text-heading-2">
-                      {user?.given_name} {user?.family_name}
-                    </p>
-
-                    <LogoutLink className="text-subtle">Log out</LogoutLink>
                   </div>
-                </div>
-              )}
-            </div>
-          </nav>
-        </header>
-        <main>{children}</main>
-        <footer className="footer">
-          <div className="container">
-            <strong className="text-heading-2">KindeAuth</strong>
-            <p className="footer-tagline text-body-3">
-              Visit our{" "}
-              <Link className="link" href="https://kinde.com/docs">
-                help center
-              </Link>
-            </p>
+                )}
+              </div>
+            </nav>
+          </header>
+          <main>{children}</main>
+          <footer className="footer">
+            <div className="container">
+              <strong className="text-heading-2">KindeAuth</strong>
+              <p className="footer-tagline text-body-3">
+                Visit our{" "}
+                <Link className="link" href="https://kinde.com/docs">
+                  help center
+                </Link>
+              </p>
 
-            <small className="text-subtle">
-              © 2025 Herit Software Limited. All rights reserved
-            </small>
-          </div>
-        </footer>
-      </body>
-    </html>
+              <small className="text-subtle">
+                © 2025 Herit Software Limited. All rights reserved
+              </small>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import {
-  authMiddleware,
-  withAuth,
-} from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export default function middleware(req: Request) {
+export default function middleware(req) {
   return withAuth(req);
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: [
+    // Run on everything but Next internals and static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ]
 };
